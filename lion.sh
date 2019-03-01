6481409225f69686433d227a37baf809a51b0fdf
nkone <phtruong@protonmail.com>
2019-03-01 15:50:46 -0800

    updated sed to work

--- lion.sh
+++ lion.sh
@@ -1,8 +1,11 @@
 #Install
 apt-get install openssh-server -y
 
+#Copy config
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.original-copy
+
 #Replace port no.
-sed -i 's/# Port 22/Port 3742/' /etc/ssh/sshd_config
+sed -i '' 's/# Port 22/Port 3742/' /etc/ssh/sshd_config
 
 #Restart service
 service sshd restart
